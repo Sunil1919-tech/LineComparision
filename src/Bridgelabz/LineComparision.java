fe37a2547a5e9e7503aafa4d03cd184c5255f0a5
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-06 00:10:14 +0530

    Added: Calculation of Coordinates Length

--- src/Bridgelabz/LineComparision.java
+++ src/Bridgelabz/LineComparision.java
@@ -1,7 +1,24 @@
 package Bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparision  {
     public static void main(String[] args) {
         System.out.println("Welcome to the Line Comparision Program");
+        Scanner sc= new Scanner((System.in));
+
+        // assigning the X coordinates
+        System.out.println("Enter the x-axis co-ordinates");
+        int x1=sc.nextInt();
+        int x2=sc.nextInt();
+        // assigning the Y coordinates
+        System.out.println("Enter the Y-axis Co-ordinates");
+        int y1=sc.nextInt();
+        int y2=sc.nextInt();
+
+        //calculating the length of two Lines
+
+        double lineLength=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("The length of Line is" + lineLength);
     }
 }
