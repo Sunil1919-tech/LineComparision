b728cc5665bcc9939070e470222c9e49d8212204
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-05 23:56:20 +0530

    Added: Line Comparision Project

---
+++ src/Bridgelabz/LineComparision.java
@@ -0,0 +1,7 @@
+package Bridgelabz;
+
+public class LineComparision  {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparision Program");
+    }
+}
