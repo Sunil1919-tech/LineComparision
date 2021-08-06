4edb4c79c93926eeebf92a131787c0898e7d7f66
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-06 12:33:52 +0530

    checkEquality_UC2

--- src/Bridgelabz/LineComparision.java
+++ src/Bridgelabz/LineComparision.java
@@ -1,5 +1,6 @@
 package Bridgelabz;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class LineComparision {
@@ -8,20 +9,45 @@ public class LineComparision {
         Scanner sc = new Scanner((System.in));
 
         // assigning the X coordinates
-        System.out.println("Enter the X-axis co-ordinates");
+        System.out.println("Enter the X-axis co-ordinates of line1");
         int x1 = sc.nextInt();
         int x2 = sc.nextInt();
 
 
         // assigning the Y coordinates
-        System.out.println("Enter the Y-axis Co-ordinates");
+        System.out.println("Enter the Y-axis Co-ordinates of line1");
         int y1 = sc.nextInt();
         int y2 = sc.nextInt();
 
 
         //calculating the length of two Lines
 
-        double lineLength = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("The length of Line is" + lineLength);
+        double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+
+        // assigning the X coordinates
+        System.out.println("Enter the X-axis co-ordinates of line1");
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+
+
+        // assigning the Y coordinates
+        System.out.println("Enter the Y-axis Co-ordinates of line1");
+        int y3 = sc.nextInt();
+        int y4 = sc.nextInt();
+        double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+
+
+        System.out.println("The length of Line1 is" + lineLength1);
+        System.out.println("The length of line2 is " +lineLength2);
+
+        if(lineLength1==lineLength2) {
+            System.out.println("Two Lines are Equal");
+        }
+        else
+         {
+            System.out.println("Two Lines are Not Equal");
+        }
+
     }
 }
